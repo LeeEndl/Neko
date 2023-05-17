9924d43c73a9fec187db86f573912ab466b2aa14
LeeEndl <127639272+LeeEndl@users.noreply.github.com>
2023-05-17 17:03:27 -0500

    integer index supportive

--- include/dpp/event_t.h
+++ include/dpp/event_t.h
@@ -13,6 +13,8 @@ namespace dpp {
 	dpp::snowflake guild_id(const dpp::slashcommand_t& event) { return event.command.guild_id; }
 	string index(const dpp::message_create_t& event, string pos) { return dpp::index(event.msg.content, ' ')[stoi(pos)]; }
 	string index(const dpp::slashcommand_t& event, string pos) { return get<string>(event.get_parameter(pos)); }
+	int64_t index_int(const dpp::message_create_t& event, string pos) { return stoull(dpp::index(event.msg.content, ' ')[stoi(pos)]); }
+	int64_t index_int(const dpp::slashcommand_t& event, string pos) { return get<int64_t>(event.get_parameter(pos)); }
 	bool empty_index(const dpp::message_create_t& event, string pos) { if (dpp::index(event.msg.content, ' ').size() == stoi(pos)) return true; else return false; }
 	bool empty_index(const dpp::slashcommand_t& event, string pos) { if (event.get_parameter("name").index() == 0) return true; else return false; }
 	dpp::message message_create(const dpp::message_create_t& event, dpp::message msg) { return bot.message_create_sync(msg); }
@@ -20,6 +22,8 @@ namespace dpp {
 	void message_edit(const dpp::message_create_t& event, dpp::message msg) { bot.message_edit_sync(msg); }
 	void message_edit(const dpp::slashcommand_t& event, dpp::message msg) { event.edit_original_response(msg); }
 	void message_delete(const dpp::message_create_t& event, dpp::snowflake msg_id, dpp::snowflake channel_id) { bot.message_delete(msg_id, channel_id); }
-	void message_delete(const dpp::slashcommand_t& event, dpp::snowflake msg_id, dpp::snowflake channel_id) { bot.message_delete(msg_id, channel_id); }
+	void message_delete(const dpp::slashcommand_t& event, dpp::snowflake msg_id, dpp::snowflake channel_id) { event.delete_original_response(); }
 	string content(const dpp::message_create_t& event) { return event.msg.content; }
+	dpp::message ori_message(const dpp::message_create_t& event) { return event.msg; }
+	dpp::message ori_message(const dpp::slashcommand_t& event) { return event.command.msg; }
 }
