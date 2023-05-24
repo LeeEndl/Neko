4e3141db8ac27bbc2f93da5c012d9e9d0b7d83d0
LeeEndl <127639272+LeeEndl@users.noreply.github.com>
2023-05-23 21:08:40 -0500

    Validate Visual Studio 2022 build tools

--- build.bat
+++ build.bat
@@ -1,6 +1,13 @@
+:: Copyright(c) LeeEndl; License Apache License 2.0
+
 @echo off
 
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 devenv "neko.sln" /build Release
+) else ( 
+@color 4
+@echo Visual Studio 2022 internal build tools not found.
+)
 
 @Pause>nul
